fb6b2aa608247b8b053ac5a666d120b5f5b2998c
Atharva Pise <atharvapise19@gmail.com>
2024-01-09 01:42:29 +0530

    feat: update types & respective styles for button component

--- src/App.tsx
+++ src/App.tsx
@@ -14,33 +14,220 @@ export default function App() {
 					title="Primary"
 					description="Primary actions should be obvious. Solid with high contrast background colors work great here."
 				>
-					<Button>Default Primary</Button>
-					<Button disabled>Disabled Primary</Button>
+					<Button>Default</Button>
+					<Button disabled>Disabled</Button>
+					{/* Primary button with override colors */}
+					<Button
+						colors={{
+							bg: "bg-amber-500",
+							hoverBg: "hover:bg-amber-600",
+							focusRing: "focus:ring-amber-500",
+						}}
+					>
+						Amber
+					</Button>
+					<Button
+						colors={{
+							bg: "bg-rose-500",
+							hoverBg: "hover:bg-rose-600",
+							focusRing: "focus:ring-rose-500",
+						}}
+					>
+						Rose
+					</Button>
+					<Button
+						colors={{
+							bg: "bg-fuchsia-500",
+							hoverBg: "hover:bg-fuchsia-600",
+							focusRing: "focus:ring-fuchsia-500",
+						}}
+					>
+						Fuchsia
+					</Button>
+					<Button
+						colors={{
+							bg: "bg-indigo-500",
+							hoverBg: "hover:bg-indigo-600",
+							focusRing: "focus:ring-indigo-500",
+						}}
+					>
+						Fuchsia
+					</Button>
+					<Button
+						colors={{
+							bg: "bg-[#1da1f2]",
+							hoverBg: "hover:bg-[#1a8cd8]",
+							focusRing: "focus:ring-[#1da1f2]",
+						}}
+					>
+						Twitter Blue
+					</Button>
 				</Section>
 
 				<Section
 					title="Secondary"
 					description="Secondary actions should be clear but not prominent. Outline styles or lower contrast background colors are great options"
 				>
-					<Button variant="secondary">Default Secondary</Button>
-					<Button variant="secondary" disabled>Disabled Secondary</Button>
+					<Button variant="secondary">Default</Button>
+					<Button variant="secondary" disabled>
+						Disabled
+					</Button>
+					<Button
+						variant="secondary"
+						colors={{
+							text: "text-amber-500",
+							hoverBorder: "border-amber-500",
+							hoverBg: "hover:bg-amber-500",
+							focusRing: "focus:ring-amber-500",
+						}}
+					>
+						Amber
+					</Button>
+					<Button
+						variant="secondary"
+						colors={{
+							text: "text-rose-500",
+							hoverBorder: "border-rose-500",
+							hoverBg: "hover:bg-rose-500",
+							focusRing: "focus:ring-rose-500",
+						}}
+					>
+						Rose
+					</Button>
+					<Button
+						variant="secondary"
+						colors={{
+							text: "text-fuchsia-500",
+							hoverBorder: "border-fuchsia-500",
+							hoverBg: "hover:bg-fuchsia-500",
+							focusRing: "focus:ring-fuchsia-500",
+						}}
+					>
+						Fuchsia
+					</Button>
+					<Button
+						variant="secondary"
+						colors={{
+							text: "text-indigo-500",
+							hoverBorder: "border-indigo-500",
+							hoverBg: "hover:bg-indigo-500",
+							focusRing: "focus:ring-indigo-500",
+						}}
+					>
+						Indigo
+					</Button>
+					<Button
+						variant="secondary"
+						colors={{
+							text: "text-[#1da1f2]",
+							hoverBorder: "border-[#1da1f2]",
+							hoverBg: "hover:bg-[#1da1f2]",
+							focusRing: "focus:ring-[#1da1f2]",
+						}}
+					>
+						Twitter Blue
+					</Button>
 				</Section>
 
 				<Section
 					title="Tertiary"
 					description="Tertiary actions should be discoverable but unobtrusive. Styling these actions like links is usually the best approach"
 				>
-					<Button variant="tertiary">Default Tertiary</Button>
-					<Button variant="tertiary" disabled>Disabled Tertiary</Button>
+					<Button variant="tertiary">Default</Button>
+					<Button variant="tertiary" disabled>
+						Disabled
+					</Button>
+					<Button
+						variant="tertiary"
+						colors={{
+							text: "text-amber-500",
+							hoverText: "hover:text-amber-700",
+							focusRing: "focus:ring-amber-500",
+						}}
+					>
+						Amber
+					</Button>
+					<Button
+						variant="tertiary"
+						colors={{
+							text: "text-rose-500",
+							hoverText: "hover:text-rose-700",
+							focusRing: "focus:ring-rose-500",
+						}}
+					>
+						Rose
+					</Button>
+					<Button
+						variant="tertiary"
+						colors={{
+							text: "text-fuchsia-500",
+							hoverText: "hover:text-fuchsia-700",
+							focusRing: "focus:ring-fuchsia-500",
+						}}
+					>
+						Fuchsia
+					</Button>
+					<Button
+						variant="tertiary"
+						colors={{
+							text: "text-indigo-500",
+							hoverText: "hover:text-indigo-700",
+							focusRing: "focus:ring-indigo-500",
+						}}
+					>
+						Indigo
+					</Button>
+					<Button
+						variant="tertiary"
+						colors={{
+							text: "text-[#1da1f2]",
+							hoverText: "hover:[#1a8cd8]",
+							focusRing: "focus:ring-[#1da1f2]",
+						}}
+					>
+						Twitter Blue
+					</Button>
 				</Section>
 
 				<Section
 					title="Destructive"
 					description="Destructive actions typically result in the removal of an item and may result in an undesirable consequence"
 				>
-					<div className="bg-blue-500/50 p-3">Component 1</div>
-					<div className="bg-blue-500/50 p-3">Component 2</div>
-					<div className="bg-blue-500/50 p-3">Component 3</div>
+					{/* Destructive primary button */}
+					<Button
+						colors={{
+							bg: "bg-red-500",
+							hoverBg: "hover:bg-red-600",
+							focusRing: "focus:ring-red-500",
+						}}
+					>
+						Yes, I'm sure I want to delete this.
+					</Button>
+
+					{/* Destructive secondary button */}
+					<Button
+						variant="secondary"
+						colors={{
+							text: "text-red-500",
+							hoverBorder: "hover:border-red-500",
+							hoverBg: "hover:bg-red-500",
+							focusRing: "focus:ring-red-500",
+						}}
+					>
+						Unsubscribe
+					</Button>
+
+					{/* Destructive tertiary button */}
+					<Button
+						variant="tertiary"
+						colors={{
+							text: "text-gray-500",
+							hoverText: "hover:text-gray-700",
+							focusRing: "focus:ring-gray-500",
+						}}
+					>
+						Cancel
+					</Button>
 				</Section>
 			</Container>
 			<Container
--- src/components/library/Button.tsx
+++ src/components/library/Button.tsx
@@ -1,33 +1,76 @@
 import { twMerge } from "tailwind-merge"
 
-type BaseButtonProps = React.ButtonHTMLAttributes<HTMLButtonElement>
-
-type ButtonProps = BaseButtonProps & {
-	variant?: "primary" | "secondary" | "tertiary"
+// define exlcusive color prop types for each variant
+type PrimaryColors = {
+	bg?: string
+	hoverBg?: string
+	focusRing?: string
 }
 
-export const Button: React.FC<ButtonProps> = ({
+type SecondaryColors = {
+	text?: string
+	hoverBorder?: string
+	hoverBg?: string
+	focusRing?: string
+}
+
+type TertiaryColors = {
+	text?: string
+	hoverText?: string
+	focusRing?: string
+}
+
+// Define base props
+type BaseButtonProps = React.ButtonHTMLAttributes<HTMLButtonElement>
+
+// Overload signatures for Button component
+type ButtonComponent = {
+	(
+		props: BaseButtonProps & { variant?: "primary"; colors?: PrimaryColors },
+	): JSX.Element
+	(
+		props: BaseButtonProps & { variant: "secondary"; colors?: SecondaryColors },
+	): JSX.Element
+	(
+		props: BaseButtonProps & { variant: "tertiary"; colors?: TertiaryColors },
+	): JSX.Element
+}
+
+export const Button: ButtonComponent = ({
 	variant = "primary",
+	colors = {},
 	...props
 }) => {
 	const baseClasses: string =
 		"inline-flex items-center justify-center gap-2 rounded-md px-4 text-sm font-medium transition-all focus:outline-none focus:ring-2 focus:ring-offset-2 disabled:pointer-events-none disabled:opacity-50"
 
-	const generateVariantClasses = (variant: string) => {
+	const generateVariantClasses = (variant: string, colorProps: any) => {
 		switch (variant) {
 			case "primary":
-				return "border border-transparent bg-emerald-500 py-3 text-white hover:bg-emerald-600 focus:ring-emerald-500"
+				return `border border-transparent ${
+					colorProps.bg || "bg-emerald-500"
+				} py-3 text-white ${colorProps.hoverBg || "hover:bg-emerald-600"} ${
+					colorProps.focusRing || "focus:ring-emerald-500"
+				}`
 			case "secondary":
-				return "border-2 border-gray-200 py-[0.688rem] text-emerald-500 hover:border-emerald-500 hover:bg-emerald-500 hover:text-white focus:ring-emerald-500"
+				return `border-2 border-gray-200 py-[0.688rem] ${
+					colorProps.text || "text-emerald-500"
+				} ${colorProps.hoverBorder || "hover:border-emerald-500"} ${
+					colorProps.hoverBg || "hover:bg-emerald-600"
+				} hover:text-white ${colorProps.focusRing || "focus:ring-emerald-500"}`
 			case "tertiary":
-				return "border border-transparent py-3 underline-offset-4 ring-offset-white hover:underline text-emerald-500 hover:text-emerald-700 focus:ring-emerald-500"
+				return `border border-transparent py-3 underline-offset-4 ring-offset-white hover:underline ${
+					colorProps.text || "text-emerald-500"
+				} ${colorProps.hoverText || "hover:text-emerald-700"} ${
+					colorProps.focusRing || "focus:ring-emerald-500"
+				}`
 			default:
 				return ""
 		}
 	}
 
 	// Generate variant class based on extracted color props
-	const variantClasses = generateVariantClasses(variant)
+	const variantClasses = generateVariantClasses(variant, colors)
 
 	// Merge baseClasses with variant Classes and any additional classes
 	const mergedClasses = twMerge([baseClasses, variantClasses])
